b34fc862e6c9afce085d293c837bec95c14e9861
David Colatti <davidcolatti@gmail.com>
2020-11-18 16:33:50 -0500

    feat: declaring variables for c#

---
+++ variables_cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variables_cs
+{
+    class Program
+    {
+        struct Person
+        {
+            int age;
+            string firstName;
+            string lastName;
+        }
+
+        static void Main(string[] args)
+        {
+            int age = 0;
+            const int NUM_MONTHS = 12;
+
+
+            age = 10;
+    
+        }
+    }
+}
